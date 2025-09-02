0db0f2ae59bebe494288f24e465fece4fa9402e6
guede <guedesanallyce4@gmail.com>
2025-09-01 23:03:08 -0300

    Atualização das páginas e inclusão de imagens

--- TrackLite/DetalhePage.xaml.cs
+++ TrackLite/DetalhePage.xaml.cs
@@ -1,9 +1,22 @@
-namespace TrackLite;
+using Microsoft.Maui.Controls;
 
-public partial class DetalhePage : ContentPage
+namespace TrackLite
 {
-    public DetalhePage()
+    public partial class DetalhePage : ContentPage
     {
-        InitializeComponent();
+        public DetalhePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync(); // Volta pra página anterior
+        }
+
+        private async void OnDownloadClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Download", "Aqui você faria o download do conteúdo.", "OK");
+        }
     }
 }
