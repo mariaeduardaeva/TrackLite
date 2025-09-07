d71531d079ee032f410b2805cde428d3980ca74c
Maria Eduarda <m.e.evangelistaa@gmail.com>
2025-09-07 20:40:41 -0300

    pagina mais iniciada

--- TrackLite/ConfigPage.xaml.cs
+++ TrackLite/ConfigPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace TrackLite
 {
     public partial class ConfigPage : ContentPage
@@ -5,6 +7,12 @@ namespace TrackLite
         public ConfigPage()
         {
             InitializeComponent();
+        }
+
+        private async void OnGitHubTapped(object sender, EventArgs e)
+        {
+            var url = "https://github.com/mariaeduardaeva/TrackLite/tree/master";
+            await Launcher.Default.OpenAsync(url);
         }
     }
 }
