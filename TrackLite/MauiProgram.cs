572292de0546bbfacdcc5c69341af919929add4b
Maria Eduarda <m.e.evangelistaa@gmail.com>
2025-09-08 10:06:52 -0300

    mudança na UI de algumas paginas

--- TrackLite/MauiProgram.cs
+++ TrackLite/MauiProgram.cs
@@ -14,6 +14,8 @@ namespace TrackLite
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Inter-Regular.ttf", "InterRegular");
+                    fonts.AddFont("Inter-Bold.ttf", "InterBold");
                 });
 
 #if DEBUG
