797c596042188a1928698512c0193f8c768bebb3
blenda <blendayudle@gmail.com>
2025-09-18 21:57:57 -0300

    bug do email feito

--- TrackLite/ConfigPage.xaml.cs
+++ TrackLite/ConfigPage.xaml.cs
@@ -1,7 +1,6 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.ApplicationModel;
 using System;
-using System.Collections.Generic;
 
 namespace TrackLite
 {
@@ -12,23 +11,29 @@ namespace TrackLite
             InitializeComponent();
         }
 
-        // Abre o repositorio do github
+        // Abre o repositório do GitHub
         private async void OnGitHubTapped(object sender, EventArgs e)
         {
             var url = "https://github.com/mariaeduardaeva/TrackLite/tree/master";
             await Launcher.Default.OpenAsync(url);
         }
 
-        //  Abre email para contatar suporte
+        // Abre email para contatar suporte
         private async void OnFeedbackClicked(object sender, EventArgs e)
         {
-            var email = new EmailMessage
+            try
             {
-                Subject = "Feedback - TrackLite",
-                Body = "",
-                To = new List<string> { "suportetracklite@gmail.com" }
-            };
-            await Email.Default.ComposeAsync(email);
+                // Cria a URL mailto com assunto e destinatário
+                var mailtoUrl = "mailto:suportetracklite@gmail.com?subject=Feedback - TrackLite";
+
+                // Abre a lista de apps disponíveis no Android ou iOS
+                await Launcher.Default.OpenAsync(new Uri(mailtoUrl));
+            }
+            catch (Exception ex)
+            {
+                // Caso algo dê errado
+                await DisplayAlert("Erro", $"Não foi possível abrir o email: {ex.Message}", "OK");
+            }
         }
 
         // Navega para a página legal
